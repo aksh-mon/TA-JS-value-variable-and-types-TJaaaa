2ded4fbbc832fae24a1fe8ff2e5b815f1958bbcf
Akshay <akshay4444.ak@gmail.com>
2022-08-17 13:45:55 +0530

    Block 4 -Assignment level 3

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -15,4 +15,38 @@ var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
 
+
+function calculateTax(amount) {
+    return amount * TAX_RATE;
+}
+
+function formatAmount(amount) {
+    return "$" + amount.toFixed( 2 );
+}
+
+// keep buying phones while you still have money
+while (amount < bank_balance) {
+    // buy a new phone!
+    amount = amount + PHONE_PRICE;
+
+    // can we afford the accessory?
+    if (amount < SPENDING_THRESHOLD) {
+        amount = amount + ACCESSORY_PRICE;
+    }
+}
+
+// don't forget to pay the government, too
+amount = amount + calculateTax( amount );
+
+console.log(
+    "Your purchase: " + formatAmount( amount )
+);
+// Your purchase: $334.76
+
+// can you actually afford this purchase?
+if (amount > bank_balance) {
+    console.log(
+        "You can't afford this purchase. :("
+    );
+}
 // ⛑ Answer of the above will `$334.76`.
